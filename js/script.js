9b475c58fc3b856bea30d71537d896c9a3c1c817
imjord <dean.jay.goat@gmail.com>
2022-06-14 19:38:21 -0600

    finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,57 @@
-var userNumber = prompt("Enter how many numbers you want to generate");
-var userArray = [];
 
-for(var i = 0; i < userNumber; i++){
-    userArray[i] = Math.floor(Math.random() * 100);
+
+ulEl = document.getElementById("list");
+
+function start(){
+    var userArray = [];
+    var userNumber = prompt("Enter how many numbers you want to generate");
+    
+    var dash = "";
+    
+    if (userNumber > 8) {
+    
+    alert("Please enter a number less than 8");
+    var userNumber = prompt("Enter how many numbers you want to generate");
+        if (userNumber > 8) {
+            alert("really bro");
+        }
+    }   
+
+    for(var i = 0; i < userNumber; i++){
+        userArray[i] = Math.floor(Math.random() * 100);
+    }
+    
+    // display the userArray
+    for(var i = 0; i < userArray.length; i++){
+     
+        if(i == 0){
+            dash += userArray[i];
+        } else {
+            dash += "-" + userArray[i];
+        }
+    
+    }
+    buttonEl = document.querySelector("button").addEventListener('click', function(){
+        var userArray = [];
+        var dash = "";
+        for(var i = 0; i < userNumber; i++){
+            userArray[i] = Math.floor(Math.random() * 100);
+        }
+        
+        // display the userArray
+        for(var i = 0; i < userArray.length; i++){
+         
+            if(i == 0){
+                dash += userArray[i];
+            } else {
+                dash += "-" + userArray[i];
+            }
+        
+        }
+        ulEl.textContent = dash;
+    });
+    ulEl.textContent = dash;
 }
+
+
+start();
